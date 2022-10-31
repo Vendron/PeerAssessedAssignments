c68ba3debbaf5f9f0b66a6b314da3979b023c25b
Jordan D <you@example.com>
2022-10-31 17:16:25 +0000

    CSV Parsing Test Code - done on a dummy page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import './style.css';
+import Papa from "papaparse";
 
 
 function App() {
+
+  //CSV Parsing below (shows data in console)
+  const chandler = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.data)
+      },
+    });
+  };
+
   return (
-    <div><header className="text-red-800">header</header></div>
+    <div>
+      {/* File Uploader */}
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={chandler}
+        style={{ display: "block", margin: "10px auto" }}
+      />
+    </div>
   );
 }
 
